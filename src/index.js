8f942c299bbbb8a566c5ecc7c639519d936fa111
jefferson oliveira <jefinho.moura14@gmail.com>
2018-04-17 23:29:35 -0300

    fixing scroll

--- src/index.js
+++ src/index.js
@@ -62,7 +62,7 @@ class JHS extends React.Component {
 const Body = styled.div`
   background-color: #000;
   width: 100%;
-  height: 100vh;
+  height: 99vh;
   font-family: 'Share Tech Mono', monospace;
   color: #fff;
   padding: 1px;
