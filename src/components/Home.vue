56f53109af2f885f48f99f8989fb0c8309e0e705
rodrigondec <rodrigondec@gmail.com>
2019-10-20 00:23:16 -0300

    vue project started

---
+++ src/components/Home.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="home col-md-6 offset-md-3">
+    <h1>Jerimum HackerSpace</h1>
+    <p>
+       Somos um grupo de pessoas interessadas em usar, remixar e compartilhar tecnologia, aprendizado, diversão e cultura de forma colaborativa e indiscriminada.
+    </p>
+    <p>
+      Temos como prioridade estratégica a manutenção de um hackerspace em Natal/RN para concretizar essas aspirações.
+    </p>
+    <p>
+      Quer falar com a gente? Quer participar? É fácil nos achar. :) 
+    </p>
+    <div>
+      <a href='https://wiki.hackerspaces.org/Jerimum_HackerSpace'>
+        <i class="fab fa-wikipedia-w"></i>
+      </a> 
+      | 
+      <a href='https://t.me/JerimumHS'>
+        <i class="fab fa-telegram-plane"></i>
+      </a> 
+      | 
+      <a href='https://twitter.com/JerimumHS'>
+        <i class="fab fa-twitter"></i>
+      </a> 
+      | 
+      <a href='https://www.facebook.com/JerimumHS/'>
+        <i class="fab fa-facebook-f"></i>
+      </a> 
+      | 
+      <a href='https://groups.google.com/forum/#!forum/hackerspace-natal'>
+        <i class="fab fa-google"></i>
+      </a>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+/* h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+} */
+</style>
