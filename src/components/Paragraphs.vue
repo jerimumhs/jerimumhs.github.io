fa59d6f355827cd1af9318d4f14de9bcd8a18d37
rodrigondec <rodrigondec@gmail.com>
2019-11-15 17:10:24 -0300

    i18n added

--- src/components/Paragraphs.vue
+++ src/components/Paragraphs.vue
@@ -1,14 +1,8 @@
 <template>
     <div>
-        <p>
-            Somos um grupo de pessoas interessadas em usar, remixar e compartilhar tecnologia, aprendizado, diversão e cultura de forma colaborativa e indiscriminada.
-        </p>
-        <p>
-            Temos como prioridade estratégica a manutenção de um hackerspace em Natal/RN para concretizar essas aspirações.
-        </p>
-        <p>
-            Quer falar com a gente? Quer participar? É fácil nos achar. :) 
-        </p>
+        <p>{{ $t('paragraphOne') }}</p>
+        <p>{{ $t('paragraphTwo') }}</p>
+        <p>{{ $t('paragraphThree') }}</p>
     </div>
 </template>
 
---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  pluginOptions: {
+    i18n: {
+      locale: 'pt-BR',
+      fallbackLocale: 'pt-BR',
+      localeDir: 'locales',
+      enableInSFC: false
+    }
+  }
+}
