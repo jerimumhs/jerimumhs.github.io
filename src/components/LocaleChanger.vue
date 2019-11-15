47fde8b867fdc2d5e7d965e2aaf8f6de99273bba
rodrigondec <rodrigondec@gmail.com>
2019-11-15 17:20:43 -0300

    LocaleChanger working with simple dict

--- src/components/LocaleChanger.vue
+++ src/components/LocaleChanger.vue
@@ -1,12 +1,9 @@
 <template>
 	<div>
-		<a v-for="obj in locales" v-bind:key="obj['locale']" v-on:click="changeLocale(obj['locale'])" href="#">
-			{{obj['languageName']}} |
+		<a v-for="(value, key, index) in locales" 
+		v-bind:key="key" v-on:click="changeLocale(key)" href="#">
+			{{value}}<span v-if="index != Object.keys(locales).length - 1"> |</span>
 		</a>
-    
-		<!-- <select v-model="$i18n.locale">
-			<option v-for="(lang, i) in langs" :key="`Lang${i}`" :value="lang">{{ lang }}</option>
-		</select> -->
 	</div>
 </template>
 
@@ -14,23 +11,20 @@
 export default {
 	name: 'LocaleChanger',
 	data () {
-		const locales = []
+		const locales = {}
 		const LanguageObjects = require.context('../locales', true, /[A-Za-z0-9-_,\s]+\.json$/i)
 		LanguageObjects.keys().forEach(key => {
 			const matched = key.match(/([A-Za-z0-9-_]+)\./i)
 			if (matched && matched.length > 1) {
 				const locale = matched[1]
-				locales.push({
-					"locale": locale,
-					"languageName": LanguageObjects(key).languageName
-				})
+				locales[locale] = LanguageObjects(key).languageName
 			}
 		})
 		return {locales: locales}
 	},
 	methods: {
 		changeLocale: function(locale) {
-			this.$root.$i18n.locale = locale
+			this.$i18n.locale = locale
 		}
 	}
 }
