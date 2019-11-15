53a3197ccb7c21b8ce9e3dc98c95a08294738fef
rodrigondec <rodrigondec@gmail.com>
2019-11-15 15:57:04 -0300

    boot with paragraphs

---
+++ src/components/Boot.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p>
+            Somos um grupo de pessoas interessadas em usar, remixar e compartilhar tecnologia, aprendizado, diversão e cultura de forma colaborativa e indiscriminada.
+        </p>
+        <p>
+            Temos como prioridade estratégica a manutenção de um hackerspace em Natal/RN para concretizar essas aspirações.
+        </p>
+        <p>
+            Quer falar com a gente? Quer participar? É fácil nos achar. :) 
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Paragraphs'
+}
+</script>
