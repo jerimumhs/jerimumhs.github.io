ad1a279c2c57c93c203819be81d2ab4e0ec50296
rodrigondec <rodrigondec@gmail.com>
2019-11-15 15:56:41 -0300

    Home using components

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,60 +1,30 @@
 <template>
-  <div class="home col-md-6 offset-md-3">
-    <h1>Jerimum HackerSpace</h1>
-    <p>
-       Somos um grupo de pessoas interessadas em usar, remixar e compartilhar tecnologia, aprendizado, diversão e cultura de forma colaborativa e indiscriminada.
-    </p>
-    <p>
-      Temos como prioridade estratégica a manutenção de um hackerspace em Natal/RN para concretizar essas aspirações.
-    </p>
-    <p>
-      Quer falar com a gente? Quer participar? É fácil nos achar. :) 
-    </p>
-    <div>
-      <a href='https://wiki.hackerspaces.org/Jerimum_HackerSpace'>
-        <i class="fab fa-wikipedia-w"></i>
-      </a> 
-      | 
-      <a href='https://t.me/JerimumHS'>
-        <i class="fab fa-telegram-plane"></i>
-      </a> 
-      | 
-      <a href='https://twitter.com/JerimumHS'>
-        <i class="fab fa-twitter"></i>
-      </a> 
-      | 
-      <a href='https://www.facebook.com/JerimumHS/'>
-        <i class="fab fa-facebook-f"></i>
-      </a> 
-      | 
-      <a href='https://groups.google.com/forum/#!forum/hackerspace-natal'>
-        <i class="fab fa-google"></i>
-      </a>
-    </div>
-
-  </div>
+	<div class="home col-md-6 offset-md-3">
+		<GlitchLogo />
+		<GlitchText />
+		<Paragraphs />
+		<LinkIcons />
+	</div>
 </template>
 
 <script>
+import GlitchLogo from './GlitchLogo'
+import GlitchText from './GlitchText'
+import Paragraphs from './Paragraphs'
+import LinkIcons from './LinkIcons'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    GlitchLogo,
+	GlitchText,
+	Paragraphs,
+    LinkIcons
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-/* h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-} */
+
 </style>
